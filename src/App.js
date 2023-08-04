db8b068ba9621d09ca186bcf13507110d3ea57b4
Zeetukumar <Zeetukr05@gmail.com>
2023-08-05 03:54:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,68 @@
 
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
+import{
+  BrowserRouter,
+  Routes,
+  Route,
+  // Link
+}from "react-router-dom";
+
 function App() {
+  // whether dark mode is enabled or not 
+  const [mode,setMode]=useState('light');
+  const [alert,setAlert]=useState(null);
+
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
+  const toggleMode=()=>{
+    if(mode==='light'){
+    setMode('dark');
+    document.body.style.backgroundColor='#403838';
+    document.body.style.color='white';
+    showAlert("Dark mode has been enabled","success");
+    document.title='TextUtils - Dark Mode';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      document.body.style.color='black';
+      showAlert("Light mode has been enabled","success");
+      document.title='TextUtils - Light Mode';
+    }
+  }
+
   return (
     <>
       
-<Navbar title="textUtils" aboutText="About TextUtils"/>
-
+{/* <Navbar title="TextUtils" aboutText="About"/> */}
+<BrowserRouter>
+<Navbar title ="textUtils" mode ={mode} toggleMode ={toggleMode}/>
+<Alert alert={alert}/>
 <div className="container my-3">
-<TextForm heading ="Enter the text"/>
+<Routes>
+{/* <Route path="/" element={<Layout />}> */}
+  <Route exact path="/about" element={<About/>}>
+    
+  </Route>
+  <Route exact path="/" element ={<TextForm showAlert={showAlert} heading ="Enter the text"mode={mode}/>}>
+  {/* <TextForm showAlert={showAlert} heading ="Enter the text"mode={mode}/> */}
+  </Route>
+</Routes>
+{/* <About/> */}
 </div>
+</BrowserRouter>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,30 +1,50 @@
 import React,{useState} from 'react'
-
-
-
 export default function TextForm(props) {
     const handleUpClick=()=>{
         // console.log("uppercase was clicked" + text); 
         let newText=text.toUpperCase();
         setText(newText);
+        props.showAlert("Converted into upperCase","success");
     }
+    const handleLowClick=()=>{
+      // console.log("uppercase was clicked" + text); 
+      let newText=text.toLowerCase();
+      setText(newText);
+      props.showAlert("Converted to LowerCase","success");
+  }
+  const handleclearClick=()=>{
+    let newText='';
+    setText(newText);
+    props.showAlert("Text has been cleared","success");
+}
     const handleOnChange=(event)=>{
         // console.log("On Change");
         setText(event.target.value);
     }
-    const [text,setText]=useState('Enter text here');
+    const [text,setText]=useState('');
     // wrong way to change  the state 
     // text="new text";
 
     // right way to change the state
     // setText("New Text") ;
   return (
-    <div>
+    <>
+    <div className="container" style ={{color:props.mode==='dark'?'white':'black'}}>
     <h1>{props.heading}</h1>
 <div className="mb-3">
-  <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
+  <textarea className="form-control" value={text} onChange={handleOnChange} style ={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'black'}} id="mybox" rows="8"></textarea>
 </div>
-<button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+<button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+<button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to Lowercase</button>
+<button className="btn btn-primary mx-1" onClick={handleclearClick}>Clear text</button>
     </div>
+    <div className="container">
+      <h1>Your text summary</h1>
+      <p>{text.split(" ").length}words ,{text.length} characters</p>
+      <p>{0.008 *text.split("").length}Minutes read</p>
+      <h2>Preview</h2>
+      <p>{text.length>0?text:"Enter Something in the textbox to preview "}</p>
+    </div>
+  </>
   )
 }
